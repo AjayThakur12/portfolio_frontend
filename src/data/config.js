a0d831f15cada6dcba5e7f5bcde1efa63d22a3b3
Ajay Singh <ajays@playsimple.in>
2021-05-19 05:49:28 +0530

    linkedin link updated

--- src/data/config.js
+++ src/data/config.js
@@ -3,6 +3,6 @@ module.exports = {
   greeting: "Hey 👋",
   greetingDescription: "I'm Ajay Singh and I'm a Software Engineer!",
   githubUrl: "https://github.com/AjayThakur12",
-  linkedinUrl: "www.linkedin.com/in/singhajay7",
+  linkedinUrl: "https://www.linkedin.com/in/singhajay7/",
   cvLink: "https://drive.google.com/file/d/1w33QDqm0jSnligUXgMRxO1xGnqhl3RDq/view?usp=sharing",
 };
